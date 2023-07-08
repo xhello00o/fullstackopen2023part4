396ccecb4b903df77050c457392b9bd371e4a957
xhello00o <133972573+xhello00o@users.noreply.github.com>
2023-07-08 12:50:36 +0800

    update test and change blogschema to accomodate part7 bloglist

--- blog_list/controller/testrouter.js
+++ blog_list/controller/testrouter.js
@@ -1,6 +1,25 @@
 const testrouter = require('express').Router()
 const Blog = require("../models/bloglist")
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
+const userList = [
+  {
+    username: "test1",
+    name: "Michael Chan",
+    password: "password123",
+  },
+  {
+    username: "test2",
+    name: "Edsger W. Dijkstra",
+    password: "pw",
+  },
+  {
+    username: "test3",
+    name: "Robert C. Martin",
+    password: "passwordis123",
+  },
+];
 
 const initialBlogs = [
   {
@@ -54,14 +73,50 @@ const initialBlogs = [
 ]
 
 
-testrouter.post("/reset", async (request,response,next)=>{
-    const blogres = await Blog.deleteMany({})
-    console.log("🚀 ~ file: testrouter.js:10 ~ testrouter.post ~ blogres:", blogres)
-    const userres = await User.deleteMany({})
-    console.log("🚀 ~ file: testrouter.js:11 ~ testrouter.post ~ userres:", userres)
-    
-    
-    response.status(204).end()
-})
+testrouter.post("/reset", async (request, response, next) => {
+  const blogres = await Blog.deleteMany({});
+  console.log(
+    "🚀 ~ file: testrouter.js:10 ~ testrouter.post ~ blogres:",
+    blogres
+  );
+  const userres = await User.deleteMany({});
+  console.log(
+    "🚀 ~ file: testrouter.js:11 ~ testrouter.post ~ userres:",
+    userres
+  );
+  let counter = 0;
+  
+  for (let user of userList) {
+    console.log("🚀 ~ file: testrouter.js:66 ~ testrouter.post ~ counter:", counter)
+    const passwordHash = await bcrypt.hash(user.password, 10);
+    const newuser = new User({
+      username: user.username,
+      name: user.name,
+      passwordHash: passwordHash,
+    });
+    const savedUser = await newuser.save();
+    let blogid = [];
+    for (let blogIndex of [counter, counter + 1]) {
+      console.log("🚀 ~ file: testrouter.js:78 ~ testrouter.post ~ blogIndex:", blogIndex)
+      let blog = new Blog({
+        ...initialBlogs[blogIndex],
+        user: savedUser._id,
+      });
+      const blogresp = await blog.save();
+      blogid.push(blogresp._id);
+    }
+    console.log("blogid", blogid);
+
+    const updateduser = {
+      _id: savedUser._id,
+      blogs: blogid,
+    };
+    await User.findByIdAndUpdate(savedUser._id, updateduser);
+
+    counter += 2;
+  }
+
+  response.status(204).end();
+});
 
 module.exports =testrouter
--- blog_list/models/bloglist.js
+++ blog_list/models/bloglist.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
     title: String,
-    author: String,
     url: String,
     likes: Number,
     user: {
